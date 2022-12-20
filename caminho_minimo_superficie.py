9028a64ad05bdee9a1e5a474eb8c14c263538e76
Claudio <58093686+ClaudioValdezRibeiro@users.noreply.github.com>
2022-12-20 20:00:02 -0300

    Update caminho_minimo_superficie.py

--- caminho_minimo_superficie.py
+++ caminho_minimo_superficie.py
@@ -1,40 +1,68 @@
-# IMPORTAR AS BIBLIOTECAS
+# IMPORTAR AS BIBLIOTECAS NECESSÁRIAS
 import random
 import matplotlib.pyplot as plt
 import numpy as np
 import networkx as nx
 from sklearn import neighbors
-from sklearn.decomposition import PCA
-
+from queue import PriorityQueue
 
 # CONTANTES
 TOTAL_PONTOS = 100
+PONTO_INICIAL = 0
+PONTO_FINAL = 1
 
 
-# E FUNÇÕES
+# FUNÇÕES AUXILIARES
 def paraboloide_hiperbolico(u, v):
   return np.array([u, v, u**2-v**2])
 
-def normal_PH(u, v):
-  d = np.sqrt(1 + 4*v**2 + 4*u**2)
-  return np.array([-2*u / d, 2*v / d, 1.0 / d])
+def dijkstra(grafo, inicial, final):
+  anterior = dict()
+  visitados = set()
+  dist = {vertice: float('inf') for vertice in grafo.nodes()}
+  dist[inicial] = 0
 
-def angulo_interno(a, b):
-  return np.arccos((a[0]*b[0]+a[1]*b[1]+a[2]*b[2])/(modulo(a)*modulo(b)))
+  pq = PriorityQueue()
+  pq.put((dist[inicial], inicial))
 
-def modulo(v):
-  return (v[0]**2 + v[1]**2 + v[2]**2)**0.5
+  while not pq.empty():
+    dist_atual, atual = pq.get()
+
+    visitados.add(atual)
+
+    if atual == final:
+      break
+
+    for vizinho in dict(grafo.adjacency()).get(atual):
+      nova_dist = dist_atual + grafo[atual][vizinho]['weight']
+      if nova_dist < dist[vizinho]:
+        dist[vizinho] = nova_dist
+        anterior[vizinho] = atual
+
+        if vizinho not in visitados:
+          pq.put((dist[vizinho], vizinho))
+   
+
+  vertice = final
+  caminho = []
+  while vertice != inicial:
+      caminho.append(vertice)
+      vertice = anterior[vertice]
+  caminho.append(vertice) 
+  caminho.reverse()
+  return (dist[final], caminho)
 
 
 # GERA PONTOS ALEATORIOS NO ESPAÇO 2D E SALVA EM "pontos_aleatorios.txt"
-arquivo = open('pontos_aleatorios.txt','w')
+# arquivo = open('pontos_aleatorios.txt','w')
 
-for i in range(TOTAL_PONTOS):
-  u = random.uniform(-1,1)
-  v = random.uniform(-1,1)
-  arquivo.write(f'{u};{v}\n')
+# for i in range(TOTAL_PONTOS):
+#   u = random.uniform(-1,1)
+#   v = random.uniform(-1,1)
+#   arquivo.write(f'{u};{v}\n')
 
-arquivo.close()
+# arquivo.close()
+
 
 dados = np.loadtxt('pontos_aleatorios.txt', delimiter=';')
 
@@ -42,8 +70,6 @@ dados = np.loadtxt('pontos_aleatorios.txt', delimiter=';')
 # APLICA OS PONTOS GERADOS NA SUPERFÍCIE
 superficie = paraboloide_hiperbolico(dados[:,0],dados[:,1])
 superficie = superficie.T
-normal_matematico = normal_PH(dados[:,0],dados[:,1])
-normal_matematico = normal_matematico.T
 
 
 # GERA UM GRAFO CONEXO COM A SUPERFÍCIE DISCRETA
@@ -58,44 +84,30 @@ while True:
   total_vizinhos += 1
 
 
-# CALCULA O VETOR NORMAL PARA CADA PONTO
-normal_discreto = np.ndarray((TOTAL_PONTOS, 3))
-for i in range(TOTAL_PONTOS):
-  vizinhanca = list(grafo[i].keys()) + [i]
-
-  pca = PCA(n_components=3)
-  pca.fit(superficie[vizinhanca])
-
-  normal = pca.components_[2]
-  normal_discreto[i] = vetor_unitario(normal)
-
-
-# CALCULA A MEDIA E DESVIO PADRAO COM BASE NO ANGULO ENTRE O DISCRETO E O MATECATICO
-diferencas = np.ndarray((TOTAL_PONTOS))
-for i in range(TOTAL_PONTOS):
-  theta = min(angulo_interno(normal_discreto[i], normal_matematico[i]),\
-              angulo_interno(-normal_discreto[i], normal_matematico[i]))
-
-  diferencas[i] = theta
-
-media = np.mean(diferencas)
-desvio_padrao = np.std(diferencas)
-
-
-
-
+distancia, caminho_minimo = dijkstra(grafo, PONTO_INICIAL, PONTO_FINAL)
+  
+  
+  
 figura = plt.figure()
 eixos = figura.add_subplot(111, projection="3d")
+arestas_caminho_minimo = list(zip(caminho_minimo, caminho_minimo[1:]))
 
-# arestas = np.array([(superficie[u], superficie[v]) for u, v in grafo.edges()])
-# for aresta in arestas:
-#     eixos.plot(*aresta.T, color="tab:gray")
-# eixos.scatter(*superficie.T, s=10, c="r")
+# var_u = np.outer(np.linspace(-1, 1, 50), np.ones(50)) 
+# var_v = var_u.copy().T 
+# eixos.plot_surface(*paraboloide_hiperbolico(var_u, var_v),color='lightgray') 
 
-eixos.quiver(*superficie.T, *normal_matematico.T, length=0.5, color=(0, 0, 1))
-eixos.quiver(*superficie.T, *normal_discreto.T, length=0.5, color=(1, 0, 0))
+for aresta in np.array([[superficie[u], superficie[v]] for u, v in grafo.edges()]):
+  eixos.plot(*aresta[0:2].T, lw=1, color='tab:gray')
 
-print("Cada ponto tem no mínimo {} vizinhos para formar um grafo continuo".format(TOTAL_PONTOS))
-print("A média de erros angulares é de {} rad com um desvio padrão de {} rad".format(media, desvio_padrao))
+aresta_superficie = np.array([[superficie[u], superficie[v]] for u, v in arestas_caminho_minimo])
+gradiante = np.linspace(0, 1, len(aresta_superficie))
+
+for aresta, t in zip(aresta_superficie, gradiante):
+  eixos.plot(*aresta.T, lw=5, color=(t,0,1-t))
+
+print("Cada ponto tem no mínimo {} vizinhos para formar um grafo conexo.".format(total_vizinhos))
+print("A distância encontrada foi de {}.".format(distancia))
+#print("seu caminho é dado por {}.".format(' -> '.join(map(str,caminho))))
+print("E este caminho é representado saindo do azul para o vermenho no gráfico.")
 
 plt.show()
