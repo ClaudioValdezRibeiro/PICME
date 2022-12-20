26894b655f570828e5fc328d72be3f23cb7e3fe2
Claudio <58093686+ClaudioValdezRibeiro@users.noreply.github.com>
2022-12-20 20:00:20 -0300

    Update normais_superficie_discreta.py

--- normais_superficie_discreta.py
+++ normais_superficie_discreta.py
@@ -19,23 +19,11 @@ def normal_PH(u, v):
   d = np.sqrt(1 + 4*v**2 + 4*u**2)
   return np.array([-2*u / d, 2*v / d, 1.0 / d])
 
-def produto_vetorial(a, b):
-  return np.array([a[1] * b[2] - a[2] * b[1], a[2] * b[0] - a[0] * b[2], a[0] * b[1] - a[1] * b[0]])
-
 def angulo_interno(a, b):
   return np.arccos((a[0]*b[0]+a[1]*b[1]+a[2]*b[2])/(modulo(a)*modulo(b)))
 
-def vetor_unitario(v):
-  if (v == [0, 0, 0]).all == True:
-    return np.array([0, 0, 0])
-
-  return v / modulo(v)
-
 def modulo(v):
   return (v[0]**2 + v[1]**2 + v[2]**2)**0.5
-
-def mapLinear(i, i0, i1, f0, f1):
-  return (i - i0) * (f1 - f0) / (i1 - i0) + f0 
 
 
 # GERA PONTOS ALEATORIOS NO ESPAÇO 2D E SALVA EM "pontos_aleatorios.txt"
@@ -49,6 +37,7 @@ for i in range(TOTAL_PONTOS):
 arquivo.close()
 
 dados = np.loadtxt('pontos_aleatorios.txt', delimiter=';')
+
 
 # APLICA OS PONTOS GERADOS NA SUPERFÍCIE
 superficie = paraboloide_hiperbolico(dados[:,0],dados[:,1])
@@ -69,20 +58,19 @@ while True:
   total_vizinhos += 1
 
 
+# CALCULA O VETOR NORMAL PARA CADA PONTO
 normal_discreto = np.ndarray((TOTAL_PONTOS, 3))
 for i in range(TOTAL_PONTOS):
-  # Pego o ponto i e seus vizinhos
   vizinhanca = list(grafo[i].keys()) + [i]
 
-  # Aplicando a Analise de Componentes Principais (PCA) em i
-  pca = PCA(n_components=3) # em 3D
+  pca = PCA(n_components=3)
   pca.fit(superficie[vizinhanca])
 
-  # Pego o ultimo vetor (onde os dados estam menos espalhados)
   normal = pca.components_[2]
   normal_discreto[i] = vetor_unitario(normal)
 
 
+# CALCULA A MEDIA E DESVIO PADRAO COM BASE NO ANGULO ENTRE O DISCRETO E O MATECATICO
 diferencas = np.ndarray((TOTAL_PONTOS))
 for i in range(TOTAL_PONTOS):
   theta = min(angulo_interno(normal_discreto[i], normal_matematico[i]),\
@@ -93,20 +81,19 @@ for i in range(TOTAL_PONTOS):
 media = np.mean(diferencas)
 desvio_padrao = np.std(diferencas)
 
-# Cria uma figura 3d
+
+
+
 figura = plt.figure()
 eixos = figura.add_subplot(111, projection="3d")
-
 
 # arestas = np.array([(superficie[u], superficie[v]) for u, v in grafo.edges()])
 # for aresta in arestas:
 #     eixos.plot(*aresta.T, color="tab:gray")
+# eixos.scatter(*superficie.T, s=10, c="r")
 
 eixos.quiver(*superficie.T, *normal_matematico.T, length=0.5, color=(0, 0, 1))
 eixos.quiver(*superficie.T, *normal_discreto.T, length=0.5, color=(1, 0, 0))
-# eixos.scatter(*superficie.T, s=10, c="r")
-
-
 
 print("Cada ponto tem no mínimo {} vizinhos para formar um grafo continuo".format(TOTAL_PONTOS))
 print("A média de erros angulares é de {} rad com um desvio padrão de {} rad".format(media, desvio_padrao))
